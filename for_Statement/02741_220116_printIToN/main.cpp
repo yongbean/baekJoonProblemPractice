a05e45ade1a3a3eeb27a8752f12380fcc65e4e15
yongbean <97684710+yongbean@users.noreply.github.com>
2022-01-16 23:39:17 +0900

    Create main.cpp

---
+++ for_Statement/02741_220116_printIToN/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+    
+    int num;
+    
+    cin >> num;
+    
+    for(int i = 1; i <= num; i++){
+        cout << i << "\n";
+    }
+    return 0;
+}
